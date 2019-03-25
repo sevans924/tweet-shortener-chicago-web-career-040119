a6bc40d98d574fedac097b5b712e98643ca269d6
Sarah Elzinga <sevans924@gmail.com>
2019-03-25 16:30:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+  :hello => 'hi',
+  :to => '2',
+  :two => '2',
+  :too =>  '2', 
+  :for => '4',
+  :four =>  '4',
+  :be => 'b',
+  :you =>  'u',
+  :at =>  "@", 
+  :and => "&"
+}
+
+def word_substituter(tweet)
+  # Write your code here.
