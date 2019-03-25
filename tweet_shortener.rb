86e6997d4371b2220446befc4ee76fdf33343582
Sarah Elzinga <sevans924@gmail.com>
2019-03-25 17:22:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,7 +39,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[1...136] + "..."
+    word_substituter(tweet)[1...137] + "..."
 else
   tweet
 end
