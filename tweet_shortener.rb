7b28b2114f0d2d11e418da3bc6df0df4dce64ff3
Sarah Elzinga <sevans924@gmail.com>
2019-03-25 16:50:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,7 +16,7 @@ end
 def word_substituter(tweet)
   tweet.split.map do |word|
     if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcas][value]
+      word = dictionary[word.downcase]
     else
       word
     end
