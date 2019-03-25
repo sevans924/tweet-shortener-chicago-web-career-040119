73a14775a29350d718c4a492fa5b3598f1830be0
Sarah Elzinga <sevans924@gmail.com>
2019-03-25 16:30:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,12 +3,12 @@ def dictionary
   :hello => 'hi',
   :to => '2',
   :two => '2',
-  :too =>  '2', 
+  :too =>  '2',
   :for => '4',
   :four =>  '4',
   :be => 'b',
   :you =>  'u',
-  :at =>  "@", 
+  :at =>  "@",
   :and => "&"
 }
 
